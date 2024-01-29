94cd62f7629665e72b37a92b5e33ef672265195e
Lucas Alves <lucas.nascimento.al16@gmail.com>
2024-01-29 00:50:44 -0300

    ajustes na função de hora

--- projetoNivel.ino
+++ projetoNivel.ino
@@ -8,23 +8,24 @@
 #define H HIGH
 
 //PORTAS
-#define RELE 2
+#define RELE 31
 #define BUZZER 7
 #define SENSOR_NIVEL_BOMBA_BAIXO 3
 #define SENSOR_NIVEL_BOMBA_CIMA 4
 #define ULTRASSONICO_CIMA_TRIG 12
 #define ULTRASSONICO_CIMA_ECHO 11
+#define LED_BOMBA 52
 
 LiquidCrystal_I2C lcd(0x27,16,2);
 UltraSonicDistanceSensor ultrassonico(ULTRASSONICO_CIMA_TRIG,ULTRASSONICO_CIMA_ECHO);
 RTC_DS3231 rtc;
 
 // (3600 * HORA) + (60 * MINUTO) + SEGUNDO
-const int HORARIO_INICIO_PERMITIDO = (3600 * 7) + (60 * 0) + 0;
-const int HORARIO_FIM_PERMITIDO = (3600 * 21) + (60 * 30) + 0;
+const long HORARIO_INICIO_PERMITIDO =  ((3600L * 7) + (60L * 0) + 0L); //25.200
+const long HORARIO_FIM_PERMITIDO = ((3600L * 23) + (60L * 30) + 0L);  //77.400
 
 //MEDIDAS
-#define ALTURA_CAIXA_DAGUA 71 //cm
+#define ALTURA_CAIXA_DAGUA 70 //cm
 #define ALTURA_TAMPA_CAIXA 12 //cm
 
 
@@ -41,6 +42,10 @@ char daysOfTheWeek[7][12] = {"Domingo","Segunda-Feira","Terça-Feira","Quarta-Fe
 DateTime agora;
 void attDataHoraPercentual(DateTime dataHora, int percentualCaixaDagua);
 
+float leituraAtualNormalizada = 0;
+float leituraAtualBruta = 0;
+int percentualCaixaMedia = 0;
+
 void setup() {
   Serial.begin(9600);
 
@@ -52,8 +57,11 @@ void setup() {
 
   //RETIRAR QUANDO FOR PARA PRODUCAO
   rtc.adjust(DateTime(__DATE__,__TIME__));  
-  // pinMode(12,O);
+  pinMode(RELE,O);
+  pinMode(LED_BOMBA,O);
+  // digitalWrite(RELE,L);
   // pinMode(11,I);
+  digitalWrite(LED_BOMBA,L);
   
   lcd.init();
   lcd.backlight();
@@ -64,29 +72,47 @@ void setup() {
   agora = rtc.now();
    
 }
+bool horarioPermite = false;
 
 void loop() {
   agora = rtc.now();
-  attDataHoraPercentual(agora,45);
-  if(horarioPermiteExecutar(agora) ){
-     
-    Serial.print("Permite");
+  leituraAtualBruta = ultrassonico.measureDistanceCm();
+  leituraAtualNormalizada = constrain(leituraAtualBruta,ALTURA_TAMPA_CAIXA,  ALTURA_CAIXA_DAGUA);
+  percentualCaixaMedia = 100 - map(mediaDescartadora(leituraAtualNormalizada),ALTURA_TAMPA_CAIXA ,ALTURA_CAIXA_DAGUA, 0, 100);
+  attDataHoraPercentual(agora,percentualCaixaMedia);
+ 
+  if(horarioPermiteExecutar(agora) && percentualCaixaMedia < 40 ){     
+    Serial.print(" -> LIGADO <- ");
+    digitalWrite(RELE,LOW);
+    digitalWrite(LED_BOMBA,H);
+    Serial.print(percentualCaixaMedia);
+    Serial.print("%");
   }else {
-    // Serial.print("Nao permite\n");
-  }
+    Serial.print(" -> DESLIGADO <-  ");
+    digitalWrite(RELE, HIGH);
+    digitalWrite(LED_BOMBA,L);
+    Serial.print(percentualCaixaMedia);
+    Serial.print("%");
+  }  
 
-  Serial.print("\n ============== MEDICOES ==============\n");
-  Serial.print("Sensor: ");
-  Serial.print(ultrassonico.measureDistanceCm());
+  Serial.print("\n");
+  Serial.print("Bruta: ");
+  Serial.print(leituraAtualBruta);
+  Serial.print(" Normalizada: ");  
+  Serial.print(leituraAtualNormalizada);
   Serial.print(" - Media: ");
-  Serial.print(mediaDescartadora(ultrassonico.measureDistanceCm()));
+  Serial.print(mediaDescartadora(leituraAtualNormalizada));  
   
   delay(500);
 }
 
 bool horarioPermiteExecutar(DateTime horaAtual){
-  int horaAtualParaSegundos = (horaAtual.hour() * 3600) + (horaAtual.minute() * 60) + horaAtual.second();  
-  (horaAtualParaSegundos >= HORARIO_INICIO_PERMITIDO && horaAtualParaSegundos < HORARIO_FIM_PERMITIDO) ? true : false;
+  long horaAtualParaSegundos = (horaAtual.hour() * 3600L) + (horaAtual.minute() * 60L) + horaAtual.second();  
+  
+  if((horaAtualParaSegundos >= HORARIO_INICIO_PERMITIDO && horaAtualParaSegundos < HORARIO_FIM_PERMITIDO))  
+    return true;
+  else     
+    return false;  
 }
 
 void attDataHoraPercentual(DateTime dataHora, int percentualCaixaDagua) {  
@@ -98,10 +124,16 @@ void attDataHoraPercentual(DateTime dataHora, int percentualCaixaDagua) {
   lcd.print(dataHora.month() % 10);
   lcd.print("/");
   lcd.print(dataHora.year());
-  lcd.setCursor(13,0);
-  lcd.print(percentualCaixaDagua/10 % 10);
-  lcd.print(percentualCaixaDagua % 10);
-  lcd.print("%");  
+  if (percentualCaixaDagua == 100){
+    lcd.setCursor(12,0);
+    lcd.print("100%");
+  }else{
+    lcd.setCursor(12,0);
+    lcd.print(" ");
+    lcd.print(percentualCaixaDagua/10 % 10);
+    lcd.print(percentualCaixaDagua % 10);
+    lcd.print("%");  
+  }
   lcd.setCursor(0,1);
   lcd.print(dataHora.hour()/10 % 10);
   lcd.print(dataHora.hour() % 10);
